ba30e5a3dbb09f2834f9b5e7003dac8c3652d9e2
DmitriIli <31270678+DmitriIli@users.noreply.github.com>
2018-04-02 19:50:03 +0300

    Update ConvertList.java

--- chapter_002/src/main/ru/job4j/selection/ConvertList.java
+++ chapter_002/src/main/ru/job4j/selection/ConvertList.java
@@ -1,10 +1,17 @@
 package ru.job4j.selection;
 
 public class Convertlist{
-  private 
   public List<Integer> toList (int[][] array) {
-    for(int[] arr: )
+    List<Integer> list = new ArrayList<Integer>();
+    for(int[] arr: array){
+     for (int i: arr){
+      this.list.add(i);
+      } 
+     }
+    return list;
   }
   public int[][] toArray (List<Integer> list, int rows) {
+   int[][] int_arr  = new int[rows][rows];
+   for ()
   }
 }
