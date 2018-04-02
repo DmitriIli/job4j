f85e38a53059f512a4202839c159464ee99a1c65
DmitriIli <31270678+DmitriIli@users.noreply.github.com>
2018-04-02 10:59:59 +0300

    Create ConvertList.java

---
+++ chapter_002/src/main/ru/job4j/selection/ConvertList.java
@@ -0,0 +1,10 @@
+package ru.job4j.selection;
+
+public class Convertlist{
+  private 
+  public List<Integer> toList (int[][] array) {
+    for(int[] arr: )
+  }
+  public int[][] toArray (List<Integer> list, int rows) {
+  }
+}
