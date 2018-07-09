af11aa9b466400f7d5bf78ad355c773fdce9a78a
DmitriIli <di.grebenev@outlook.com>
2018-07-09 16:32:07 +0300

    strategy

---
+++ chapter_002/src/test/java/ru/job4j/strategy/StrategyTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.strategy;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StrategyTest {
+
+    @Test
+    public void whenDrawSquare() {
+        Square square = new Square();
+        assertThat(
+                square.draw(),
+                is(
+                        new StringBuilder()
+                                .append("++++")
+                                .append("+  +")
+                                .append("+  +")
+                                .append("++++")
+                                .toString()
+                )
+        );
+    }
+    @Test
+    public void whenDrawTriangle() {
+        Triangle triangle= new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(
+                        new StringBuilder()
+                                .append("  +  ")
+                                .append(" + + ")
+                                .append("+   +")
+                                .append("+++++")
+                                .toString()
+                )
+        );
+    }
+}
+
